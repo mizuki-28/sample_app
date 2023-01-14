4166329742b1ffa11a8c0d5f5165d75eedcd683e
mizuki <mizuki@example.com>
2023-01-14 16:11:45 +0000

    Add user microposts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -101,4 +101,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # アップロードされたファイルをAWSに保存する
+  config.active_storage.service = :amazon
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,15 +2,6 @@ require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
-  def setup
-    @base_title = "Ruby on Rails Tutorial Sample App"
-  end
-
-  test "should get root" do
-    get root_path
-    assert_response :success
-  end
-
   test "should get home" do
     get root_path
     assert_response :success
@@ -22,17 +13,16 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
   end
-  
+
   test "should get about" do
     get about_path
     assert_response :success
     assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
-  
+
   test "should get contact" do
     get contact_path
     assert_response :success
     assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
   end
-  
 end
